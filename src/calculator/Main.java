89ae3d19f48a143ee2923311bfbbe2babfda7317
Igor <Iriday@mail.ru>
2019-12-10 18:49:40 +0200

    Added possibility to read an unlimited sequence of numbers from the standard input and calculate their sum.

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -6,22 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a;
-        int b;
+        //int a;
+        //int b;
         String[] input;
 
+        int sum = 0;
         while (true) {
             input = scanner.nextLine().split(" ");
 
-            if (input.length == 2) {
-                a = Integer.parseInt(input[0]);
-                b = Integer.parseInt(input[1]);
-                System.out.println(a + b);
+            if (input.length >= 2) {
+                for (String num : input) {
+                    try {
+                        sum += Integer.parseInt(num);
+                    } catch (Exception e) {
+                        break;
+                    }
+                }
+                System.out.println(sum);
+                sum = 0;
 
             } else if (input.length == 1) {
                 if ("/exit".equalsIgnoreCase(input[0])) {
                     System.out.println("Bye!");
                     break;
+
+                } else if ("/help".equalsIgnoreCase(input[0])) {
+                    System.out.println("The program calculates the sum of numbers");
+
                 } else {
                     try {
                         System.out.println(Integer.parseInt(input[0]));
