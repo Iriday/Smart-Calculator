45f33564903b88439de0fe8130c3658a826f96f0
Igor <Iriday@mail.ru>
2019-12-10 18:08:43 +0200

    Added loop

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -6,9 +6,30 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        int a;
+        int b;
+        String[] input;
 
-        System.out.println(a + b);
+        while (true) {
+            input = scanner.nextLine().split(" ");
+
+            if (input.length == 2) {
+                a = Integer.parseInt(input[0]);
+                b = Integer.parseInt(input[1]);
+                System.out.println(a + b);
+
+            } else if (input.length == 1) {
+                if ("/exit".equalsIgnoreCase(input[0])) {
+                    System.out.println("Bye!");
+                    break;
+                } else {
+                    try {
+                        System.out.println(Integer.parseInt(input[0]));
+                    } catch (Exception e) {
+                        continue;
+                    }
+                }
+            }
+        }
     }
 }
