d0fe8262383f0b654eeccfbe2214c6491a2d2ced
Igor <Iriday@mail.ru>
2019-12-16 10:13:54 +0200

    Add */()^. Use stack/queue/postfix notation to calculate expressions

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -1,21 +1,17 @@
 package calculator;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
-    enum Operators {ADD, SUBTRACT, MULTIPLY, DIVIDE, EMPTY}
+    /*enum Operators {ADD, SUBTRACT, MULTIPLY, DIVIDE, EMPTY}*/
 
     private final Pattern numberOrWordPattern = Pattern.compile("\\d+|[a-zA-Z]+");
-    private final Pattern numberPattern = Pattern.compile("([-+]?\\d+|[a-zA-Z]+)");
+    //private final Pattern numberPattern = Pattern.compile("([-+]?\\d+|[a-zA-Z]+)");
     private final Pattern wordPattern = Pattern.compile("[a-zA-Z]+");
     private Matcher matcher;
-    //private Pattern operator;
-    private Operators operators = Operators.EMPTY;
-    private final String actionsRegex = "[^+-]";
+    //private final String actionsRegex = "[^+-]";
     private final Map<String, Integer> variables = new HashMap<>();
 
     public static void main(String[] args) {
@@ -31,48 +27,34 @@ public class Main {
         int b;
 
         while (true) {
-            input = scanner.nextLine();
+            input = scanner.nextLine().replaceAll(" +", "");
 
+            if (input.isEmpty()) {
+                continue;
+            }
             if ("/exit".equalsIgnoreCase(input)) {
                 System.out.println("Bye!");
                 break;
             } else if ("/help".equalsIgnoreCase(input)) {
-                System.out.println("The program can add/subtract numbers, and supports variables.\n" +
-                        "Examples: -2 + 4 - 5 + 6, 9 +++ 10 -- 8, 3 --- 5, 14  -   12, a = 2, a + -3 -5 + 2 + a");
+                System.out.println("The program can add/subtract/multiply/divide numbers, supports parenthesis, variables, power operator.\n" +
+                        "Examples: 9 +++ 10 -- 8 * 3 / 2,\n" +
+                        "3 + 8 * ((4 + 3) * 2 + 1) - 6 / (2 + 1),\n" +
+                        "a = 2,\n" +
+                        "b = 3,\n" +
+                        "a + -3 -5 + (b + a),\n" +
+                        "2 * 2^3.");
                 continue;
             }
 
             if (!validInput(input)) {
                 continue;
             }
-
             if (input.matches(" *[a-zA-Z]+ *= *(\\d+|[a-zA-Z]+).*")) {
                 addVariable(input);
                 continue;
             }
-            matcher = numberPattern.matcher(input);
-            if (!matcher.find()) {
-                continue;
-            }
-            String val1 = input.substring(matcher.start(), matcher.end());
-            a = valueOf(val1);
 
-            for (; true; ) {
-                input = input.substring(matcher.end());
-
-                matcher = numberOrWordPattern.matcher(input);
-                if (matcher.find()) {
-                    String val2 = input.substring(matcher.start(), matcher.end());
-                    b = valueOf(val2);
-
-                    String actions = input.substring(0, matcher.start()).replaceAll(actionsRegex, "");//.split
-                    a = result(actions, a, b);
-                    //System.out.println(a);
-                } else {
-                    break;
-                }
-            }
-            System.out.println(a);
+            System.out.println(compute(infixToPostfix(compressOperators(input))));
         }
     }
 
@@ -95,36 +77,152 @@ public class Main {
         return a;
     }
 
-    private int result(String actions, int a, int b) {
-        int result = -1;
-
-        for (char action : actions.toCharArray()) {
-            if (action == '+') {
-                operators = operators == Operators.EMPTY || operators == Operators.ADD ? Operators.ADD : Operators.SUBTRACT;
-
-            } else if (action == '-') {
-                operators = operators == Operators.EMPTY || operators == Operators.ADD ? Operators.SUBTRACT : Operators.ADD;
-            }
-        }
-
-        switch (operators) {
-            case ADD:
-                result = add(a, b);
-                break;
-            case SUBTRACT:
-                result = subtract(a, b);
-                break;
-        }
-        operators = Operators.EMPTY;
-        return result;
-    }
-
     public static int add(int a, int b) {
         return a + b;
     }
 
     public static int subtract(int a, int b) {
         return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static int power(int a, int b) {
+        int result = a;
+        for (int i = 1; i < b; i++) {
+            result *= a;
+        }
+        return result;
+    }
+
+    public static String compressOperators(String input) {
+        Pattern plusesPattern = Pattern.compile("[+]{2,}");
+        Matcher matcher = plusesPattern.matcher(input);
+        input = matcher.replaceAll("+");
+
+        Pattern pattern1 = Pattern.compile("[-]{2,}");
+        matcher = pattern1.matcher(input);
+        while (matcher.find()) {
+            String val = matcher.group();
+            if (val.length() % 2 == 0) {
+                input = input.replaceAll(val, "+");
+            } else {
+                input = input.replaceAll(val, "-");
+            }
+            matcher = pattern1.matcher(input);
+        }
+        return input;
+    }
+
+    public static Queue<String> infixToPostfix(String infix) {
+        //String postFix = "";
+        //Stack<String> stack = new Stack<>();
+        Deque<String> stack = new ArrayDeque<>();
+        Queue<String> queue = new ArrayDeque<>();
+
+        Pattern pattern = Pattern.compile(
+                "(^[+[-]]\\d+)|" +
+                        "([(][+[-]]\\d+[)])|" +
+                        "([\\da-zA-Z]+)|" +
+                        "([+[-]/*()^])");
+        Matcher matcher = pattern.matcher(infix);
+
+
+        while (matcher.find()) {
+            String inputElement = matcher.group();
+
+            if (inputElement.matches("[(][+[-]]\\d+[)]")) {
+                queue.offer(inputElement.replaceAll("[()]", ""));
+
+            } else if (inputElement.matches("[\\da-zA-Z]+|[+[-]]\\d+")) {
+                queue.offer(inputElement);
+
+            } else if (inputElement.matches("[(]")) {
+                stack.offer(inputElement);
+
+            } else if (inputElement.matches("[)]")) {
+                while (!stack.getLast().equals("(")) {
+                    queue.offer(stack.removeLast());
+                }
+                stack.removeLast();
+
+            } else if (inputElement.matches("[+[-]]")) {
+                //System.out.println("+ or -");
+                if (stack.isEmpty()) {
+                    stack.offer(inputElement);
+                } else {
+                    String stackElement = stack.getLast();
+                    if ("(".equals(stackElement)) {
+                        stack.offer(inputElement);
+                    } else {
+                        while (!stack.isEmpty() && !"(".equals(stack.getLast())) {
+                            queue.offer(stack.removeLast());
+                        }
+                        stack.offer(inputElement);
+                    }
+                }
+            } else if (inputElement.matches("[*/]")) {
+                if (stack.isEmpty()) {
+                    stack.offer(inputElement);
+                } else {
+                    String stackElement = stack.getLast();
+                    if ("(".equals(stackElement)) {
+                        stack.offer(inputElement);
+                    } else if ("+".equals(stackElement) || "-".equals(stackElement)) {
+                        stack.offer(inputElement);
+                    } else {
+                        while (!stack.isEmpty() && !stack.getLast().matches("[+[-](]")) {
+                            queue.offer(stack.removeLast());
+                        }
+                        stack.offer(inputElement);
+                    }
+                }
+            } else if (inputElement.matches("\\^")) {
+                stack.offer(inputElement);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            queue.offer(stack.removeLast());
+        }
+        //System.out.println(queue);
+        return queue;
+    }
+
+    public int compute(Queue<String> inputInPostfix) {
+        Stack<Integer> result = new Stack<>();
+
+        for (String element : inputInPostfix) {
+            if (element.matches("^[+[-]]\\d+|[\\da-zA-Z]+")) {
+                result.push(valueOf(element));
+            } else if (element.equals("+")) {
+                int a = result.pop();
+                int b = result.pop();
+                int value = add(a, b);
+                result.push(value);
+            } else if (element.equals("-")) {
+                int a = result.pop();
+                int b = result.pop();
+                result.push(subtract(b, a));
+            } else if (element.equals("*")) {
+                result.push(multiply(result.pop(), result.pop()));
+            } else if (element.equals("/")) {
+                int a = result.pop();
+                int b = result.pop();
+                result.push(divide(b, a));
+            } else if (element.equals("^")) {
+                int a = result.pop();
+                int b = result.pop();
+                result.push(power(b, a));
+            }
+        }
+        return result.peek();
     }
 
     private boolean validInput(String input) {
@@ -138,27 +236,27 @@ public class Main {
             return false;
         }
         // check the beginning of the string
-        if (!input.matches("^ *[-+]?[\\da-zA-Z].*")) {//".*[\\da-zA-Z]+.*[-+=]+.*[\\da-zA-Z]+ *"////
+        if (!input.matches("^[(]*[-+]?[(]*[\\da-zA-Z].*")) {
             System.out.println("Invalid expression");
             return false;
         }
         // check the end of the string
-        if (!input.matches(".*[\\da-zA-Z] *$")){
+        if (!input.matches(".*[\\da-zA-Z][)]*$")) {
             System.out.println("Invalid expression");
             return false;
         }
         if (input.matches(" *[-+]?\\d+ *")) {
             return true;
         }
-        if(input.matches(" *[a-zA-Z]+ *")){
-            String variable =input.trim();
-            if(variables.containsKey(variable)) {
+        if (input.matches(" *[a-zA-Z]+ *")) {
+            String variable = input.trim();
+            if (variables.containsKey(variable)) {
                 return true;
-            }else
+            } else
                 System.out.println("Unknown variable");
             return false;
         }
-        if (input.matches(".*[^\\d +[-]=a-zA-Z].*")) {
+        if (input.matches(".*[^\\d +[-]=a-zA-Z*/()^].*")) {
             System.out.println("Invalid expression");
             return false;
         }
@@ -196,10 +294,14 @@ public class Main {
                 if (!variables.containsKey(matcher.group())) {
                     System.out.println("Unknown variable");
                     return false;
-                    // break;
                 }
             }
         }
+        if (input.matches(".*[*]{2,}.*|.*[/]{2,}.*|.*[*&&/].*|.*[/&&*].*")) {
+            System.out.println("Invalid assignment");
+            return false;
+        }
+
         int index = input.indexOf("=");
         if (index != -1) {
             if (input.matches(" *[a-zA-Z]+ *= *[\\da-zA-Z]+ *")) {
@@ -218,13 +320,20 @@ public class Main {
             }
         }
         //input = input.substring(index + 1);
-        Pattern pattern = Pattern.compile("[\\da-zA-Z]+( *[^+[-]] *)+?[\\da-zA-Z]+");
+        Pattern pattern = Pattern.compile("[\\da-zA-Z]+( *[^+[-]*/^] *)+?[\\da-zA-Z]+");// \\w
         matcher = pattern.matcher(input);
         if (matcher.find()) {
             System.out.println("Invalid expression");
             valid = false;
         }
 
+        if (input.contains("(") || input.contains(")")) {
+            String parenthesis = input.replaceAll("[^()]+", "");
+            if (parenthesis.length() % 2 != 0) {
+                System.out.println("Invalid expression");
+                return false;
+            }
+        }
         return valid;
     }
 }
